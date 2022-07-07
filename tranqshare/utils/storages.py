6c26c5089bc43ba31a0a26ff5b65cfa49a35b5f7
Jeremiah David <jeremiahedavid@gmail.com>
2022-07-07 21:50:53 +0300

    fixed imports

--- tranqshare/utils/storages.py
+++ tranqshare/utils/storages.py
@@ -5,7 +5,20 @@ class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = "static"
     default_acl = "public-read"
 
+    def _normalize_name(self, name):
+        return name
+        # try:
+        #     return safe_join(self.location, name)
+        # except (SuspiciousOperation, ValueError):
+        #     return ""
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
+
+    def _normalize_name(self, name):
+        return name
+        # try:
+        #     return safe_join(self.location, name)
+        # except (SuspiciousOperation, ValueError):
+        #     return ""
