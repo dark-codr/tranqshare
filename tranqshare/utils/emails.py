63cd68a8d02e0ff2794ead7d5219153e0d4dac44
Jeremiah David <jeremiahedavid@gmail.com>
2022-07-08 15:09:40 +0300

    fixed imports

--- tranqshare/utils/emails.py
+++ tranqshare/utils/emails.py
@@ -4,18 +4,18 @@ from django.core.mail import EmailMessage
 
 
 def plain_email(to_email, subject, body):
-    message = EmailMessage(subject=subject, body=body, to=[to_email], bcc=["postmaster@tranqshare.com"])
+    message = EmailMessage(subject=subject, body=body, to=[to_email], bcc=["admin@tranqshare.com"])
     message.content_subtype = "html"
     message.send()
 
 def support_email(to_email, subject, body, from_email):
-    message = EmailMessage(subject=f"New Support Mail - {subject}", body=body, to=[to_email], bcc=["postmaster@tranqshare.com"], reply_to=[from_email])
+    message = EmailMessage(subject=f"New Support Mail - {subject}", body=body, to=[to_email], bcc=["admin@tranqshare.com"], reply_to=[from_email])
     message.content_subtype = "html"
     message.send()
 
 
 def pdf_attachment_email(to_email, subject, body, filepath, filename):
-    message = EmailMessage(subject=f"New Quotation - {subject}", body=body, to=[to_email], bcc=["postmaster@tranqshare.com"], reply_to=["support@tranqshare.com"])
+    message = EmailMessage(subject=f"New Quotation - {subject}", body=body, to=[to_email], bcc=["admin@tranqshare.com"], reply_to=["support@tranqshare.com"])
     file_data = open(filepath, "rb")
     message.attach(filename, file_data.read(), "application/pdf")
     message.content_subtype = "html"
